717adef47081ea7685f5c92cb367df31e7c3f091
平凡的世界 <963559210@qq.com>
2018-04-08 21:43:28 +0800

    优化完毕 HashMap,TreeMap,LinkedHashMap

--- src/CalculateApperas.java
+++ src/CalculateApperas.java
@@ -1,26 +1,31 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
- * @ 计算一个字符串中每个字符出现的次数
+ * @author 没想法的岁月
+ * 计算一个字符串中每个字符出现的次数
  * Created by 没想法的岁月 on 2018/4/8.
  */
 public class CalculateApperas {
     public static void main(String[] args) {
-        String str = "abcdddddefghijklmnqwertabccccccccc";
+        String str = "fasdasdfdfcvsdaaaaabbb";
         char[] chars = str.toCharArray();
-        HashMap<Object, Object> map = new HashMap<>();
+//        无序的排序
+//        HashMap<Character, Integer> map = new HashMap<>();
+        //自然排序
+//        TreeMap<Character, Integer> map = new TreeMap<>();
+        //按照添加的先后顺序排序
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
         for (char ch:chars
              ) {
-            if (map.keySet().contains(ch)) {
-                map.replace(ch, (int) map.get(ch) + 1);
-            } else {
-                map.put(ch, 1);
-            }
+                if (map.keySet().contains(ch)) {
+//                    map.replace(ch,  map.get(ch)+1);
+                    Integer old = map.get(ch);
+                    map.put(ch, old + 1);
+                } else {
+                    map.put(ch, 1);
+                }
         }
-        Set<Map.Entry<Object, Object>> entries = map.entrySet();
-        System.out.println(entries);
+        System.out.println(map);
     }
 
 
